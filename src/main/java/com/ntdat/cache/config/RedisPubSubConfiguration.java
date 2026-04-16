4d077d505131dc551c2b09764a2de5f8716153d6
ntdat104 <ntd.k108cva@gmail.com>
2026-04-16 22:19:20 +0700

    fix build

--- src/main/java/com/ntdat/cache/config/RedisPubSubConfiguration.java
+++ src/main/java/com/ntdat/cache/config/RedisPubSubConfiguration.java
@@ -2,11 +2,13 @@ package com.ntdat.cache.config;
 
 import com.ntdat.cache.cache.VersionInvalidateSubscriber;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+@Configuration
 public class RedisPubSubConfiguration {
 
     public static final String VERSION_INVALIDATE_CHANNEL = "version:invalidate";
